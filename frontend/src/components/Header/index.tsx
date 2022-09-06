d5c71e66b8bf7c4f26c753b5a6e895ae66da51e1
Antônio Damous <antonio.damous@gmail.com>
2022-09-05 22:48:59 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="dsmeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.linkedin.com/in/antoniodamous/?originalSubdomain=br">
+            @antoniodamous
+          </a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header

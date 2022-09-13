99b45aeeb700657b84faa5c5aa42ea9f00473b3c
Antônio Damous <antonio.damous@gmail.com>
2022-09-12 22:55:09 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { Sale } from '../models/sale';
-import { BASE_URL } from '../utils/request';
+import { Sale } from '../../models/sale';
+import { BASE_URL } from '../../utils/request';
 import NotificationButton from '../NotificationButton';
 import './styles.css';
 
